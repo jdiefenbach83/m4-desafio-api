98233a5900cace430b875ecdd4cf9f02fbeab629
Jefferson Diefenbach <jefferson.diefenbach@uhk.com.br>
2020-08-28 22:02:22 -0300

    Initial commit

---
+++ controllers/gradeController.js
@@ -0,0 +1,135 @@
+import { logger } from '../config/logger.js';
+import { studentModel } from '../models/student.js';
+
+const create = async (req, res) => {
+  try {
+    const student = new studentModel(req.body);
+
+    await student.save();
+
+    res.send({ message: 'Grade inserido com sucesso' });
+    logger.info(`POST /grade - ${JSON.stringify()}`);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: error.message || 'Algum erro ocorreu ao salvar' });
+    logger.error(`POST /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const findAll = async (req, res) => {
+  const name = req.query.name;
+
+  //condicao para o filtro no findAll
+  var condition = name
+    ? { name: { $regex: new RegExp(name), $options: 'i' } }
+    : {};
+
+  try {
+    const students = await studentModel.find(condition);
+
+    const studentsList = students.map(({ _id, name, subject, type, value }) => {
+      return {
+        id: _id,
+        name,
+        subject,
+        type,
+        value,
+      };
+    });
+
+    res.send(studentsList);
+
+    logger.info(`GET /grade`);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: error.message || 'Erro ao listar todos os documentos' });
+    logger.error(`GET /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const findOne = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const student = await studentModel.findById(id);
+
+    const { _id, name, subject, type, value } = student;
+
+    res.send({
+      id: _id,
+      name,
+      subject,
+      type,
+      value,
+    });
+
+    logger.info(`GET /grade - ${id}`);
+  } catch (error) {
+    res.status(500).send({ message: 'Erro ao buscar o Grade id: ' + id });
+    logger.error(`GET /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const update = async (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: 'Dados para atualizacao vazio',
+    });
+  }
+
+  const id = req.params.id;
+
+  try {
+    const student = await studentModel.findOneAndUpdate(id, req.body, {
+      new: true,
+    });
+
+    if (!!!student) {
+      res.status(404).send('Documento não encontrado');
+    } else {
+      res.send(student);
+    }
+
+    logger.info(`PUT /grade - ${id} - ${JSON.stringify(req.body)}`);
+  } catch (error) {
+    res.status(500).send({ message: 'Erro ao atualizar a Grade id: ' + id });
+    logger.error(`PUT /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const remove = async (req, res) => {
+  const id = req.params.id;
+
+  try {
+    const student = await studentModel.findByIdAndDelete({ _id: id });
+
+    if (!!!student) {
+      res.status(404).send('Documento não encontrado');
+    } else {
+      res.sendStatus(200);
+    }
+
+    logger.info(`DELETE /grade - ${id}`);
+  } catch (error) {
+    res
+      .status(500)
+      .send({ message: 'Nao foi possivel deletar o Grade id: ' + id });
+    logger.error(`DELETE /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+const removeAll = async (req, res) => {
+  try {
+    await studentModel.deleteMany({});
+
+    res.sendStatus(200);
+
+    logger.info(`DELETE /grade`);
+  } catch (error) {
+    res.status(500).send({ message: 'Erro ao excluir todos as Grades' });
+    logger.error(`DELETE /grade - ${JSON.stringify(error.message)}`);
+  }
+};
+
+export default { create, findAll, findOne, update, remove, removeAll };
